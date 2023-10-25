dca5fb4020eab435fcf179024b6f38b266bf0dd4
miacias <miaciasullo@gmail.com>
2023-10-25 12:18:36 -0400

    added prompt template to help OpenAI be more accurate and specific with its responses

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const { OpenAI } = require('langchain/llms/openai');
 const inquirer = require('inquirer');
+const { PromptTemplate } = require("langchain/prompts");
 
 // instantiates a wrapper OpenAI with basic config
 const model = new OpenAI({ 
@@ -15,7 +16,17 @@ const model = new OpenAI({
 // passes in prompts to OpenAI via user inputs from Inquirer
 const promptFunc = async (input) => {
   try {
-    const res = await model.call(input);
+    // creates OpenAI prompt template
+    const prompt = new PromptTemplate({
+      template: "You are a JavaScript expert and will answer the user's coding questions as thorougly as possible.\n{question}",
+      inputVariables: ["question"],
+    });
+    // passes inuser input to prompt template
+    const promptInput = await prompt.format({
+      question: input,
+    });
+    // sends request to OpenAI with user input formatted into a template
+    const res = await model.call(promptInput);
     console.log(res);
   } catch (err) {
     console.error(err);
@@ -29,6 +40,7 @@ const init = () => {
       type: 'input',
       name: 'name',
       message: 'Ask a JavaScript coding question:',
+      // validation
     },
   ])
   .then((inquirerResponse) => {
